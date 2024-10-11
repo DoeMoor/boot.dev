264e5825052d4bc4740189ca198ded04271f3a43
DianaMoor <princestaDiana@gmail.com>
2024-10-11 15:20:54 +0200

    add unit tests for ApiCall

---
+++ internal/client/api_call_test.go
@@ -0,0 +1,44 @@
+package client
+
+import (
+	"reflect"
+	"testing"
+
+	scheme "github.com/DoeMoor/pokedexcli/internal/json_scheme"
+)
+
+type pokemon struct {
+	Name string `json:"name"`
+}
+
+func TestApiCall(t *testing.T) {
+	var loc scheme.Locations
+	var resultLoc scheme.Locations
+	var poke pokemon
+	var resultPoke pokemon
+	var urla string = "https://pokeapi.co/api/v2/location-area/?offset=0&limit=5"
+
+	t.Run(t.Name()+" matched scheme", func(t *testing.T) {
+		err := ApiCall(urla, &loc)
+		if err != nil {
+			t.Errorf("ApiCall() error = %v", err)
+			return
+		}
+		if reflect.TypeOf(loc) != reflect.TypeOf(resultLoc) {
+			t.Errorf("ApiCall() = %v, want %v", loc, resultLoc)
+			return
+		}
+	})
+
+	t.Run(t.Name()+" unmatched scheme", func(t *testing.T) {
+		err := ApiCall(urla, &poke)
+		if err != nil {
+			t.Errorf("ApiCall() error = %v", err)
+			return
+		}
+		if reflect.TypeOf(poke) == reflect.TypeOf(resultLoc) {
+			t.Errorf("ApiCall() = %v, want %v", poke, resultPoke)
+			return
+		}
+	})
+}
